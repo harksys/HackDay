c9a0dc9ac7b897ce373e0e48974f5ba03d151b54
Alex McNair <alex@harksys.com>
2019-07-18 14:46:39 +0100

    Styling and api changes for typescript sample - added tslint.json - fixed style warnings - updated metric ids based on api changes - removed api method that no longer exists

--- typescript/src/js/App.ts
+++ typescript/src/js/App.ts
@@ -1,21 +1,39 @@
 import { controls, metrics } from './Core';
+import { MetricId } from './Core/MetricClient';
 
-const hot = 20;
-const cold = 15;
+const hot: number = 20;
+const cold: number = 15;
 
-// Turn on the red/blue lamp according to the temperature
-(async () => {
-    await metrics.connect();
+const metricMap: { [key: string]: { id: MetricId; topic: string; source: string } } = {
+  'ifm-temperature': { id: 1, topic: 'temperature', source: 'IFM' },
+  'ifm-vibration': { id: 2, topic: 'vibration', source: 'IFM' },
+  'motor-rpm': { id: 3, topic: 'motor', source: 'MOTOR' },
+  'motor-frequency': { id: 4, topic: 'motor', source: 'MOTOR' },
+  'mototr-amps': { id: 5, topic: 'motor', source: 'MOTOR' },
+  'motor-watts': { id: 6, topic: 'motor', source: 'MOTOR' },
+  'mototr-volts': { id: 7, topic: 'motor', source: 'MOTOR' },
+  'epc-co2': { id: 8, topic: 'co2', source: 'EPC' },
+  'epc-temperature': { id: 9, topic: 'temperature', source: 'EPC' },
+  'epc-humidity': { id: 10, topic: 'humidity', source: 'EPC' },
+  'meter-amps': { id: 11, topic: 'energy', source: 'METER' },
+  'meter-volts': { id: 12, topic: 'energy', source: 'METER' },
+  'meter-kw': { id: 13, topic: 'energy', source: 'METER' },
+  'meter-kwh': { id: 14, topic: 'energy', source: 'METER' }
+};
 
-    console.log('Successfully connected to metric stream');
+// turn on the red/blue lamp according to the temperature
+(async (): Promise<void> => {
+  await metrics.connect();
 
-    await metrics.handle('temperature', async (temperature) => {
-        console.log(`Temperature: ${temperature.value}`);
+  console.log('Successfully connected to metric stream');
 
-        await controls.setDeviceState('redLamp', (temperature.value >= hot));
-        await controls.setDeviceState('blueLamp', (temperature.value <= cold));
-    });
-    
-    await metrics.handle('co2', async (co2) =>
-        console.log(`CO2: ${co2.value}`));
+  await metrics.handle(metricMap['epc-temperature'].id, async (temperature): Promise<void> => {
+    console.log(`Temperature: ${temperature.value}`);
+
+    await controls.setDeviceState('redLamp', (temperature.value >= hot));
+    await controls.setDeviceState('blueLamp', (temperature.value <= cold));
+  });
+
+  await metrics.handle(metricMap['epc-co2'].id, async (co2): Promise<void> =>
+    console.log(`CO2: ${co2.value}`));
 })();
