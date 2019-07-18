85514174d6c1d25f559b3116036d85a4d0595110
Alex McNair <alex.mcnair@hotmail.co.uk>
2019-07-18 16:19:14 +0100

    Updated python sample (#3)

--- python/core/config.py
+++ python/core/config.py
@@ -1,3 +1,6 @@
-control_panel_url = "http://10.0.0.45:3000/api"
-mqtt_broker_address = "10.0.0.45"
+# Note:
+# update the configuration with the ip address of the gateway
+# you have been given.
+control_panel_url = "http://[YOUR_GATEWAY_ADDRESS_HERE]:3000/api"
+mqtt_broker_address = "[YOUR_GATEWAY_ADDRESS_HERE]"
 mqtt_broker_port = 1883
--- python/run.py
+++ python/run.py
@@ -3,25 +3,32 @@ from core import metrics, controls
 hot = 20
 cold = 15
 
+tempId = 1
+co2Id = 8
+
+
 def handle_temperature(temperature):
     print(f"Temperature: {temperature.value}")
-    
+
     controls.set_device_state('redLamp', temperature.value >= hot)
     controls.set_device_state('blueLamp', temperature.value <= cold)
 
+
 def handle_co2(co2):
     print(f"CO2: {co2.value}")
+
 
 def run():
     metrics.connect()
 
     print("Successfully connected to metric stream")
 
-    metrics.handle('temperature', handle_temperature)
-    metrics.handle('co2', handle_co2)
+    metrics.handle(tempId, handle_temperature)
+    metrics.handle(co2Id, handle_co2)
 
     while True:
         pass
+
 
 if __name__ == '__main__':
     run()
